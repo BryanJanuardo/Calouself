3c802d150a71cc8d053a75b6dca2c31e0063a53e
Trinata <159982772+NataaKuri@users.noreply.github.com>
2024-12-14 13:17:51 +0700

    Dashboard Seller

--- src/Views/LoginPage.java
+++ src/Views/LoginPage.java
@@ -1,6 +1,7 @@
 package Views;
 
 import Managers.ViewManager;
+import Views.Seller.DashboardPage;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -8,6 +9,7 @@ import javafx.scene.layout.VBox;
 public class LoginPage implements Page{
 	private ViewManager viewManager;
 	private StackPane root;
+	private Button testBtn;
 
 	private VBox vbox;
 	private Button registerBtn;
@@ -24,25 +26,33 @@ public class LoginPage implements Page{
 	public void init() {
 		vbox = new VBox(10);
 		registerBtn = new Button("Register");
+		testBtn = new Button("test");
 
 	}
 	
 	@Override
 	public void setLayout() {
-		vbox.getChildren().addAll(registerBtn);
+		vbox.getChildren().addAll(registerBtn, testBtn);
 		
 		root.getChildren().add(vbox);
+		
+		
 	}
 	
 	@Override
 	public void setEvent() {
 		registerBtn.setOnAction(e -> registerPage());
+		testBtn.setOnAction(e -> testPage());
 		
 	}
 	
 	public void registerPage() {
 		viewManager.changePage(new RegisterPage(viewManager).getPage());
 	}
+	public void testPage() {
+		viewManager.changePage(new DashboardPage(viewManager).getPage());
+	}
+
 
 	@Override
 	public StackPane getPage() {
